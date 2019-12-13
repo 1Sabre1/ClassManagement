0aa6e95b0d8970b4f5d37c58e8e0a475d8126f58
1Sabre1 <sabrerians@gmail.com>
2019-12-13 18:34:06 +0300

    First version half-working

--- ClassManagement/ClassManagement/MainFormAdmin.cs
+++ ClassManagement/ClassManagement/MainFormAdmin.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
-
 namespace ClassManagement {
 	public partial class MainFormAdmin : Form {
 		/*	 1 : принят запрос
@@ -17,24 +18,90 @@ namespace ClassManagement {
 		public MainFormAdmin() {
 			InitializeComponent();
 			dataGridView1.RowCount = 15;
-			for (int i = 0; i < dataGridView1.RowCount - 1; i++) {
-				dataGridView1[0, i].Value = $"{i + 1} ауд.";
-			}
-			dataGridView1[0, dataGridView1.RowCount - 1].Value = "Конф. зал";
-			for (int i = 1; i < dataGridView1.ColumnCount; i++) {
-				for (int j = 0; j < dataGridView1.RowCount; j++) {
-					dataGridView1[i, j].Style.BackColor = busy;
-				}
-			}
-			int temp = rnd.Next(15);
-			int r = 0, c = 1;
-			while (r < 15 && c < 8) {
-				if (r % 2 == 0 || c % 2 != 0) dataGridView1[c, r].Style.BackColor = free;
-				else dataGridView1[c, r].Style.BackColor = pretend;
-				r++; c++;
-			}
-		}
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                dataGridView1[0, i].Value = $"{i + 1} ауд.";
+            }
+            dataGridView1[0, dataGridView1.RowCount - 1].Value = "Конф. зал";
+            for (int i = 1; i < dataGridView1.ColumnCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    dataGridView1[i, j].Style.BackColor = free;
+                }
+            }
+            //Thread SetInfo = new Thread(new ThreadStart(RefreshDataGrid));
+            //SetInfo.Start();
+            RefreshDataGrid();
 
+
+            //int temp = rnd.Next(15);
+            //int r = 0, c = 1;
+            //while (r < 15 && c < 8) {
+            //	if (r % 2 == 0 || c % 2 != 0) dataGridView1[c, r].Style.BackColor = free;
+            //	else dataGridView1[c, r].Style.BackColor = pretend;
+            //	r++; c++;
+            //}
+        }
+
+        public void RefreshDataGrid()//Обновляет содержимое dataGridView 
+        {
+            if (dataGridView1.InvokeRequired)
+            {
+                BeginInvoke(new Action(RefreshDataGrid));
+            }
+            else
+            { 
+                using (StepSchedulerEntities1 db = new StepSchedulerEntities1())
+                {
+                    IQueryable<ReservedRoom> Querry = from t1 in db.ReservedRooms //достаем зарезервированные аудитории где дата проведения равна дате в dateTimePicker1 
+                                                      join t2 in db.Requests on t1.RequestId equals t2.RequestId
+                                                      where(t2.Status!=-1)
+                                                      select t1;
+                    foreach (ReservedRoom item in Querry)
+                    {
+
+                        string ShortFormatItem = item.Request.ClassDate.ToShortDateString();
+                        string SelectedShortFormat = dateTimePicker1.Value.ToShortDateString();
+                        if (ShortFormatItem == SelectedShortFormat)
+                        {
+                            if (item.Request.Status == 1)
+                            {
+                                dataGridView1[item.Request.LessonNumber, item.Request.ClassRoom.Number-1].Style.BackColor = busy;
+                                if (item.EventType == 0)
+                                {
+                                    dataGridView1[item.Request.LessonNumber, item.Request.ClassRoom.Number-1].Value = "З";
+                                }
+                                if (item.EventType == 1)
+                                {
+                                    dataGridView1[item.Request.LessonNumber, item.Request.ClassRoom.Number-1].Value = "К";
+                                }
+                                if (item.EventType == 2)
+                                {
+                                    dataGridView1[item.Request.LessonNumber, item.Request.ClassRoom.Number-1].Value = "М";
+                                }
+                            }
+                            else
+                            {
+                                dataGridView1[item.Request.LessonNumber, item.Request.ClassRoom.Number-1].Style.BackColor = pretend;
+                                if (item.EventType == 0)
+                                {
+                                    dataGridView1[item.Request.LessonNumber, item.Request.ClassRoom.Number-1].Value = "З";
+                                }
+                                if (item.EventType == 1)
+                                {
+                                    dataGridView1[item.Request.LessonNumber, item.Request.ClassRoom.Number-1].Value = "К";
+                                }
+                                if (item.EventType == 2)
+                                {
+                                    dataGridView1[item.Request.LessonNumber, item.Request.ClassRoom.Number].Value = "М";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 		private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e) {
 			
 		}
@@ -83,5 +150,11 @@ namespace ClassManagement {
 			AudienceForm form = new AudienceForm();
 			form.Show();
 		}
-	}
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)//значение в dateTimePicker1 изменилось ,обновляю dataGridView 
+        {
+            Thread SetInfo = new Thread(new ThreadStart(RefreshDataGrid));
+            SetInfo.Start();
+        }
+    }
 }
